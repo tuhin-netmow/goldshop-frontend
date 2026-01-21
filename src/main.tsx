f9eb14323a08047368f80aa8d62b5bcea293b3d0
Tuhin Alom <tuhin.netmow@gmail.com>
2026-01-21 16:11:08 +0600

    fixed activity issues

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,22 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { Toaster } from './components/ui/sonner'
 
+// Protect against malicious browser extensions trying to modify React internals
+if (typeof window !== 'undefined') {
+  const originalDefineProperty = Object.defineProperty;
+  Object.defineProperty = function<T>(
+    o: T,
+    p: PropertyKey,
+    attributes: PropertyDescriptor
+  ): T {
+    try {
+      return originalDefineProperty(o, p, attributes);
+    } catch (e) {
+      console.warn('Failed to define property:', p, e);
+      return o;
+    }
+  };
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
